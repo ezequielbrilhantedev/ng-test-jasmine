163d4fbb918db1102ce202f6ecd167a4fd21aaed
ezequielbrilhante <e.ribeiro.brilhante@accenture.com>
2021-12-30 00:55:30 -0300

    primeiro commit

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,23 @@
+import { UniqueIdService } from './unique-id.service';
+
+const service = new UniqueIdService();
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+});
---
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UniqueIdService {
+  private numberOfGeneratedIds = 0;
+
+  public generateUniqueIdWithPrefix(prefix: string): string {
+    if (!prefix) {
+      throw Error('Prefix can not be empty');
+    }
+    const uniqueId = this.gereateUniqueId();
+    this.numberOfGeneratedIds++;
+    return `${prefix}-${uniqueId}`;
+  }
+
+  public getNumberOfGeneratedUniqueIds(): number {
+    return this.numberOfGeneratedIds;
+  }
+
+  private gereateUniqueId(): string {
+    return uuidv4();
+  }
+}
