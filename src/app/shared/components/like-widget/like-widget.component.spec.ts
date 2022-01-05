fbcc13d0c95d2e8abfe77e3423a5e733ca7f46ad
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-01-05 16:36:00 -0300

    Testando Output e Event Emitter

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -34,4 +34,13 @@ describe(LikeWidgetComponent.name, () => {
     expect(component.id).toBe(someId);
   })
 
+  it(`#${LikeWidgetComponent.prototype.like.name}
+    should trigger emission when called`, () => {
+    fixture.detectChanges();
+    component.liked.subscribe(() => {
+      expect(true).toBeTrue();
+    });
+    component.like();
+  })
+
 })
