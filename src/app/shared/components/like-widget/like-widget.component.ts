20fddb705270d0dbf6f0ea653967c21f7405ac0b
Ezequiel Brilhante <ezequielrb7@gmail.com>
2022-01-05 00:53:25 -0300

    Uso o componente like e config de acessibilidade

--- src/app/shared/components/like-widget/like-widget.component.ts
+++ src/app/shared/components/like-widget/like-widget.component.ts
@@ -1,13 +1,30 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { UniqueIdService } from "../../services/unique-id/unique-id.service";
 @Component({
   selector: 'app-like-widget',
   templateUrl: './like-widget.component.html',
   styleUrls: ['./like-widget.component.scss']
 })
 
-export class LikeWidgetComponent {
+export class LikeWidgetComponent implements OnInit {
   @Output() public liked = new EventEmitter();
-  @Input() public like = 0;
+  @Input() public likes = 0;
   @Input() public id = null;
+
+  public fonts = {
+    faThumbsUp
+  }
+
+  constructor(private uniqueIdService: UniqueIdService) { }
+
+  ngOnInit(): void {
+    if (!this.id) {
+      this.id = this.uniqueIdService.generateUniqueIdWithPrefix('like-widget');
+    }
+  }
+
+  like() {
+    this.liked.emit();
+  }
 }
